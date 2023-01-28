c001e29418cbf6f0e8a9a6e3a09f0c74f19deffa
league <league@imac-16.attlocal.net>
2023-01-27 16:57:48 -0800

    Level 3 Module 4 00 00-1 COMPLETE

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -10,14 +10,41 @@ public class _01_IntroToStack {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
 
+    	Stack<Double> popper = new Stack<Double>();
+    	
+    	
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 
+    	for(int i = 0; i < 100; i++) {
+    		popper.push(new Random().nextDouble()*100);
+    	}
+    	
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
 
+    	String popme = JOptionPane.showInputDialog("Enter two numbers between 0 and 100 inclusive with the first number being smaller than the second. (e.g. 76,100)");
+    	String[] popme2 = popme.split(",");
+    	
+    	int[] popme3 = new int[2]; 
+    	popme3[0] = Integer.valueOf(popme2[0]);
+    	popme3[1] = Integer.valueOf(popme2[1]);
+    	
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
 
-
+    	
+    	System.out.println("NUM 1: " + popme3[0]);
+    	System.out.println("NUM 2: " + popme3[1]);
+    	
+    	System.out.println("Elements on stack are being popped");
+    	System.out.println("Element between " + popme3[0] + " and " + popme3[1] + ":");
+    	for(int i = 0; i < 100; i++) {
+    		Double popped = popper.pop();
+    		
+    		if(popped > popme3[0] && popped < popme3[1]) {
+    			System.out.println(popped);
+    		}
+    	}
+    	
         // EXAMPLE:
         // NUM 1: 65
         // NUM 2: 75
