67ccd8071ff20bbfd0eb8c29df999b8694ae09bf
league <league@imac-38.attlocal.net>
2023-02-17 16:52:46 -0800

    Level 3 Module 4 Hangman Progress

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -2,6 +2,7 @@ package _03_Hangman;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -14,8 +15,11 @@ public class Hangman implements KeyListener{
 	JPanel panel = new JPanel();
 	JLabel underscores = new JLabel();
 	int numWords; 
-	
-	
+	String imPopped;
+	// static boolean cont = true;  idea
+	int lives;
+	ArrayList<Integer> indexes;
+	char save; 
 	
 	public static void main(String [] args) {
 		Hangman a = new Hangman();
@@ -29,13 +33,17 @@ public class Hangman implements KeyListener{
 			int d = Integer.parseInt(b);
 		}catch(NumberFormatException e) {
 			JOptionPane.showMessageDialog(null, "Game Over! Try to insert an *integer* next time.");
-			System.exit(-1);
 		} 
 		int d = Integer.parseInt(b);
 		
 		a.numWords = d;
 		
-		a.run();
+		
+		//while(cont) {
+			a.run();
+		//}
+		System.exit(-1);
+			
 	}
 	private void run() {
 		f.setVisible(true);
@@ -44,9 +52,9 @@ public class Hangman implements KeyListener{
 		panel.add(underscores);
 		
 		if(numWords < 1 || numWords > 100) {
-			JOptionPane.showMessageDialog(null, "Game Over! Try to insert a valid integer next time.");
-			System.exit(-1);
-		}
+			JOptionPane.showMessageDialog(null, "Game Over! Try to insert a valid integer next time.");		
+			}
+		
 		
 		Stack<String> words = new Stack<String>();
 		
@@ -64,7 +72,7 @@ public class Hangman implements KeyListener{
 		int lives = 10; 
 		
 		for(int i = 0; i < numWords; i++) {
-			String imPopped = words.pop();
+			imPopped = words.pop();
 			System.out.println(imPopped);
 			String tmp = "";
 			for(int k = 0; k < imPopped.length(); k++) {
@@ -75,12 +83,29 @@ public class Hangman implements KeyListener{
 		
 			
 			f.pack();
+		}
+			
+		
+		boolean wrong = true; 
+		
+		for(int i = 0; i < imPopped.length(); i++) {
+			if(save == imPopped.charAt(save)) {
+				wrong = false; 
+			}
+		}
+		
+		if(wrong && lives == 1) {
+			JOptionPane.showMessageDialog(null, "Game Over! Thanks for playing!");
+		}
+		else if(wrong){
+			lives--;
+		}
+		else {
+			
+			// RESET DISPLAY HERE
 			
 			
 		}
-		
-		
-		
 		
 		
 		
@@ -89,6 +114,8 @@ public class Hangman implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		
+		
 		
 	}
 	@Override
@@ -99,13 +126,39 @@ public class Hangman implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("keyTyped " + e.toString()) ;
+		//System.out.println("keyTyped " + e.toString()) ;						
+		
+		
+		//FUNCTION DISPLAYS WHICH KEY PRESSED
+		
+		save = e.toString().charAt(0);
+		for(int i = 0; i < imPopped.length(); i++) {
+			if(save == imPopped.charAt(i)) {
+				//indexes.add(i);
+				
+				//ENTER USEFUL STUFF HERE
+			}
+			else {
+				//indexes.add(-1); // ignore -1 value when you redisplay
+				//lives--;
+			}
+		}
+		
 		
 		
 	}
 	
-}
+
+	
+} 
+	
+		
+	
+
+	
+	
 
 
 
-// version A.2
+
+// version A.4.1
