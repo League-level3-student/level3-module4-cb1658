338cb8c3bf397a6169935621c560ac196480cb35
league <league@imac-32.attlocal.net>
2023-03-10 16:55:37 -0800

    Level 3 Module 4 MazeMaker Almost Done

--- src/_04_Maze_Maker/MazeMaker.java
+++ src/_04_Maze_Maker/MazeMaker.java
@@ -22,11 +22,38 @@ public class MazeMaker {
         // 1. Pick a random cell along the border and remove its exterior wall.
         //    This will be the starting point. Then select a random cell along
         //    the opposite wall and remove its exterior wall. This will be the
-        //    finish line.
+        //    finish line. 
+                               
+        
+        int a = randGen.nextInt(rows);
+        int a1;
+        if( a == rows -1 ) {
+        	a1 = a - 1;
+        }else {
+        	a1 = a + 1;
+        }
+        removeWalls(maze.getCell(a,0),maze.getCell(a1,0)); 
+        
+        int b = randGen.nextInt(rows);
+        int b1;
+        if( b == rows -1 ) {
+        	b1 = b - 1;
+        }else {
+        	b1 = b + 1;
+        }
+        removeWalls(maze.getCell(b,cols-1),maze.getCell(b+1,cols-1));     // keep a watch out for this 
+        
         
         // 2. select a random cell in the maze to start 
         
+        int d = randGen.nextInt(rows);
+        int e = randGen.nextInt(cols);
+        Cell f = maze.getCell(d, e);
+        
+        
         // 3. call the selectNextPath method with the randomly selected cell
+        
+        selectNextPath(f);
 
         return maze;
     }
@@ -34,32 +61,56 @@ public class MazeMaker {
     // 4. Complete the selectNextPathMethod
     private static void selectNextPath(Cell currentCell) {
         // A. SET currentCell as visited
+    	
+    	currentCell.setBeenVisited(true);
 
         // B. check for unvisited neighbors using the cell
-
+    	
+    	ArrayList<Cell> al = getUnvisitedNeighbors(currentCell);
+    	
+    	///////////////////////////
+    	
+    	if(al.size() > 0) {
+    		
+    	
+    	
         // C. if has unvisited neighbors,
-
+    		
         // C1. select one at random.
-
+    		
+    		Cell save = al.get(new Random().nextInt(al.size()));
+    		
         // C2. push it to the stack
 
+    		uncheckedCells.push(save);
+    		
         // C3. remove the wall between the two cells
 
+    			removeWalls(save, currentCell);
         // C4. make the new cell the current cell and SET it as visited
 
+    			save.setBeenVisited(true);
+    			
         // C5. call the selectNextPath method with the current cell
 
-
+    		selectNextPath(save);
+    	}
+    		
+    	
+    	else 
         // D. if all neighbors are visited
-
+    		if(!uncheckedCells.isEmpty()) {
+    			
+    		
         // D1. if the stack is not empty
 
         // D1a. pop a cell from the stack
-
+    			currentCell = uncheckedCells.pop();
         // D1b. make that the current cell
-
+    			
         // D1c. call the selectNextPath method with the current cell
-
+    			selectNextPath(currentCell);
+    		}
     }
 
     // This method will check if c1 and c2 are adjacent.
